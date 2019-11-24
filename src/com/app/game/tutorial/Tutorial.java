34d9306a169abd7e636d2b137a2fd3e5d333cbd7
root <root@patartimotius01>
2019-11-24 22:08:12 +0700

    commit

--- src/com/app/game/tutorial/Tutorial.java
+++ src/com/app/game/tutorial/Tutorial.java
@@ -2,33 +2,42 @@ package com.app.game.tutorial;
 
 import com.app.game.tutorial.input.GameInputProcessor;
 import com.app.game.tutorial.input.GameKeys;
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Tutorial extends Game {
-	private SpriteBatch batch;
+public class Tutorial extends ApplicationAdapter {
+
+
+	private SpriteBatch sb;
 	
 
 
 	private static int WIDTH;
 	private static int HEIGHT;
+
 	
-	private OrthographicCamera cam;
-	
+	private GameStateManager gsm;
 	
 	@Override
 	public void create() {
 		// TODO Auto-generated method stub
 		WIDTH = Gdx.graphics.getWidth();
-		HEIGHT = Gdx.graphics.getHeight();
-		cam =  new OrthographicCamera(WIDTH,HEIGHT);
-		cam.translate(WIDTH/2, HEIGHT/2);
-		cam.update();
+		HEIGHT = Gdx.graphics.getHeight();/*
+											 * cam = new OrthographicCamera(WIDTH,HEIGHT);
+											 */
+		/*
+		 * cam.translate(WIDTH/2, HEIGHT/2); cam.update();
+		 */
 		
+		cam = new OrthographicCamera();
+		cam.setToOrtho(false,WIDTH, HEIGHT);
+		sb = new SpriteBatch();
 		Gdx.input.setInputProcessor(new GameInputProcessor());
+		this.gsm = new GameStateManager(this);
 	}
 	@Override
 	public void dispose() {
@@ -41,13 +50,32 @@ public class Tutorial extends Game {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
-		if(GameKeys.isPressed(GameKeys.SHIFT)) {
-			System.out.println("SHIFT PRESED");
-		}
-		if(GameKeys.isPressed(GameKeys.ENTER)) {
-			System.out.println("ENTER PRESED");
-		}
+		/*
+		 * if(GameKeys.isPressed(GameKeys.SHIFT)) { System.out.println("SHIFT PRESED");
+		 * } if(GameKeys.isPressed(GameKeys.ENTER)) {
+		 * System.out.println("ENTER PRESED"); }
+		 */
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.draw();
 		GameKeys.update();
 		
 	}
+	public SpriteBatch getSb() {
+		return sb;
+	}
+	public void setSb(SpriteBatch sb) {
+		this.sb = sb;
+	}
+	
+	
+	public OrthographicCamera getCam() {
+		return cam;
+	}
+	public void setCam(OrthographicCamera cam) {
+		this.cam = cam;
+	}
+	private OrthographicCamera cam;
+	
+	
+	
 }
